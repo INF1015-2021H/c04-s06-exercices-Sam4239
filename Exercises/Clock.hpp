8cd0f05325a73e2ca63c4b8c5b8412bb94e1db5b
Sam4239 <71023108+Sam4239@users.noreply.github.com>
2021-03-18 19:33:01 -0400

    Fonctionne

--- Exercises/Clock.hpp
+++ Exercises/Clock.hpp
@@ -1,4 +1,4 @@
-///
+/// Version prof
 /// Exemples des slides avec des horloges sur l'héritage, le polymorphisme et la conversion.
 ///
 
@@ -11,6 +11,7 @@
 #include <string>
 #include <ctime>
 #include <iostream>
+#include <concepts>
 
 #include <cppitertools/itertools.hpp>
 
@@ -21,13 +22,13 @@ using namespace iter;
 class Clock {
 public:
 	Clock(bool useMilitary)
-	: isMilitary_(useMilitary) {
+		: isMilitary_(useMilitary) {
 		update();
 	}
 
-	~Clock() = default;
+	virtual ~Clock() = default;
 
-	string getLocation() const {
+	virtual string getLocation() const {
 		return "Local";
 	}
 
@@ -35,7 +36,7 @@ public:
 		return isMilitary_;
 	}
 
-	int getHours() const {
+	virtual int getHours() const {
 		return now_.tm_hour % (isMilitary_ ? 24 : 12);
 	}
 
@@ -50,33 +51,39 @@ public:
 
 	friend ostream& operator<<(ostream& out, const Clock& clock) {
 		char hm[32] = "";
-		snprintf(hm, 32, "%2i:%02i", clock.getHours(), clock.getMinutes());
+		snprintf(hm, sizeof(hm) / sizeof(hm[0]), "%2i:%02i", clock.getHours(), clock.getMinutes());
 		return out << clock.getLocation() << ": " << hm;
 	}
 
 private:
-	bool isMilitary_;
+	bool isMilitary_ = false;
 	tm   now_ = {};
 };
 
 
 class TravelClock : public Clock {
 public:
-	TravelClock (bool useMilitary, const string& location, int timeDiff)
-	: Clock(useMilitary),
-	  location_(location),
-	  timeDiff_(timeDiff) { }
+	TravelClock(bool useMilitary, const string& location, int timeDiff)
+		: Clock(useMilitary),
+		location_(location),
+		timeDiff_(timeDiff) { }
 
-	string getLocation() const {
+	~TravelClock() = default;
+
+	string getLocation() const override {
 		return location_;
 	}
 
-	int getHours() const {
-		static auto posMod = [] (int x, int n) {
+	int getHours() const override {
+		static auto posMod = [](int x, int n) {
 			return (x % n) + (x < 0 ? n : 0);
 		};
 		int h = Clock::getHours() + timeDiff_;
 		return posMod(h, isMilitary() ? 24 : 12);
+	}
+
+	int getTimeDiff() const {
+		return timeDiff_;
 	}
 
 private:
@@ -84,3 +91,110 @@ private:
 	int    timeDiff_;
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///// Ma version
+///// Exemples des slides avec des horloges sur l'héritage, le polymorphisme et la conversion.
+/////
+//
+//#pragma once
+//
+//
+//#include <cstddef>
+//#include <cstdint>
+//
+//#include <string>
+//#include <ctime>
+//#include <iostream>
+//#include <concepts>
+//
+//#include <cppitertools/itertools.hpp>
+//
+//using namespace std;
+//using namespace iter;
+//
+//
+//class Clock {
+//public:
+//	Clock(bool useMilitary)
+//	: isMilitary_(useMilitary) {
+//		update();
+//	}
+//
+//	virtual ~Clock() = default;
+//
+//	virtual string getLocation() const {
+//		return "Local";
+//	}
+//
+//	bool isMilitary() const {
+//		return isMilitary_;
+//	}
+//
+//	virtual int getHours() const {
+//		return now_.tm_hour % (isMilitary_ ? 24 : 12);
+//	}
+//
+//	int getMinutes() const {
+//		return now_.tm_min;
+//	}
+//
+//	void update() {
+//		now_.tm_hour = 14;
+//		now_.tm_min = 9;
+//	}
+//
+//	//template <derived_from <Clock> ClockType>, remplacer Clock par ClockType
+//	friend ostream& operator<<(ostream& out, const Clock& clock) {
+//		char hm[32] = "";
+//		snprintf(hm, 32, "%2i:%02i", clock.getHours(), clock.getMinutes());
+//		return out << clock.getLocation() << ": " << hm;
+//	}
+//
+//private:
+//	bool isMilitary_;
+//	tm   now_ = {};
+//};
+//
+//
+//class TravelClock : public Clock {
+//public:
+//	TravelClock (bool useMilitary, const string& location, int timeDiff)
+//	: Clock(useMilitary),
+//	  location_(location),
+//	  timeDiff_(timeDiff) { }
+//
+//	~TravelClock() = default;
+//
+//	string getLocation() const override {
+//		return location_;
+//	}
+//
+//	int getHours() const override {
+//		static auto posMod = [] (int x, int n) {
+//			return (x % n) + (x < 0 ? n : 0);
+//		};
+//		int h = Clock::getHours() + timeDiff_;
+//		return posMod(h, isMilitary() ? 24 : 12);
+//	}
+//
+//private:
+//	string location_;
+//	int    timeDiff_;
+//};
+//
